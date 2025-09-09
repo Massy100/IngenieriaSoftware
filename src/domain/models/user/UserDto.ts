5a2e3bb1cb0c6e839455f88252e9ede61350fd61
Massy100 <massielle.coti@gmail.com>
2025-09-08 21:29:11 -0600

    atributo phone del modelo user agregado

--- src/domain/models/user/UserDto.ts
+++ src/domain/models/user/UserDto.ts
@@ -5,7 +5,8 @@ export class UserDto {
     public readonly dpi: string,
     public readonly name: string,
     public readonly age: number,
-    public readonly isValid: boolean
+    public readonly isValid: boolean,
+    public readonly phone: string 
   ) {}
 
   public toJSON(): string {
@@ -15,7 +16,8 @@ export class UserDto {
       dpi: this.dpi,
       name: this.name,
       age: this.age,
-      isValid: this.isValid
+      isValid: this.isValid,
+      phone: this.phone 
     });
   }
 
@@ -26,7 +28,16 @@ export class UserDto {
     name: string;
     age: number;
     isValid: boolean;
+    phone: string; 
   }): UserDto {
-    return new UserDto(obj.id, obj.email, obj.dpi, obj.name, obj.age, obj.isValid);
+    return new UserDto(
+      obj.id,
+      obj.email,
+      obj.dpi,
+      obj.name,
+      obj.age,
+      obj.isValid,
+      obj.phone
+    );
   }
 }
