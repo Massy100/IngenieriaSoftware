2f78423197768ed4cd4f9b6a4105c8aa0ba92787
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-14 22:24:08 -0600

    fix(email): Ya se puede enviar correos

--- src/domain/models/notification/EmailNotificationSender.ts
+++ src/domain/models/notification/EmailNotificationSender.ts
@@ -6,11 +6,16 @@ import nodemailer from 'nodemailer';
 export class EmailNotificationSender implements NotificationSender {
   private transporter: nodemailer.Transporter;
   constructor() {
-    this.transporter = nodemailer.createTransport({host : process.env.SMTP_HOST,
-      port : process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
-      secure : false,
-    });
-  }
+    this.transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+}
 
   public async send(user: User): Promise<void> {
     console.log(`Sending email notification to: ${user.getEmail()}`);
@@ -19,8 +24,8 @@ export class EmailNotificationSender implements NotificationSender {
     const info = await this.transporter.sendMail({
       from: process.env.FROM_EMAIL || '',
       to: user.getEmail(),
-      subject: 'Email Notification',
-      text: `Hello ${user.getName()},\n\nYou have been verified.\n\nBest regards,\nMassielle, Daniel(*2), Karen, Carlos`
+      subject: 'Verified User',
+      text: `Welcome ${user.getName()},\n\nYour account has been succesfully validated.\n\nNow you have full access to the app features\nBest regards.`
     });
 
     console.log('Email sent: %s', info.messageId);
